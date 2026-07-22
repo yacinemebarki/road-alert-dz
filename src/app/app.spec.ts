e75cc75eb5ad9ddaf33d3e205767d76c9b9b9721
mebarkiyacine <mebarkiyacine822@gmail.com>
2026-07-22 19:09:23 +0100

    start with angular

---
+++ src/app/app.spec.ts
@@ -0,0 +1,23 @@
+import { TestBed } from '@angular/core/testing';
+import { App } from './app';
+
+describe('App', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [App],
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(App);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it('should render title', async () => {
+    const fixture = TestBed.createComponent(App);
+    await fixture.whenStable();
+    const cohttp://localhost:4200/mpiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, road-alert-dz');
+  });
+});
---
+++ src/app/app.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Home } from './home/home';
+
+@Component({
+  selector: 'app-root',
+  imports: [Home],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {}
