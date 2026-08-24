cfe6ec2befdb999439e0fe4b7e8d04d47ed3ea04
mebarkiyacine <mebarkiyacine822@gmail.com>
2026-08-24 15:18:57 +0100

    continue with verfication api

--- frontend/src/app/sign-up-inputs/sign-up-inputs.ts
+++ frontend/src/app/sign-up-inputs/sign-up-inputs.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
@@ -7,9 +8,37 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrl: './sign-up-inputs.css',
 })
 export class SignUpInputs {
-  @Output() btn =  new EventEmitter<void>();
+  @Output() UpBtn =  new EventEmitter<void>();
+
+  constructor(private http: HttpClient){}
+
+  user_name = '';
+  email = '';
+  password = '';
+  confirm_password = '';
 
   onVerify(){
-    this.btn.emit();
+    if(this.user_name == '' || this.email == '' || this.password == '' || this.confirm_password == ''){
+      return;
+    }
+
+    if(this.password.length < 8){
+      return;
+    }
+
+    if(this.password != this.confirm_password){
+      return;
+    }
+    this.UpBtn.emit();
+
+    const data = {
+      user_name: this.user_name,
+      email: this.email,
+      password: this.password
+    }
+
+    this.http.post('http://localhost:3000/api/new_user', data).subscribe(response => {
+      console.log(response);
+    });
   }
 }
--- frontend/src/app/verfication/verfication.ts
+++ frontend/src/app/verfication/verfication.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-verfication',
@@ -6,4 +7,23 @@ import { Component } from '@angular/core';
   templateUrl: './verfication.html',
   styleUrl: './verfication.css',
 })
-export class Verfication {}
+export class Verfication {
+  @Output() verfyBtn = new EventEmitter<void>();
+  code = '';
+
+  constructor(private http: HttpClient){}
+
+  verfication(){
+    if(this.code == ''){
+      return;
+    }
+
+    const data = {
+      code: this.code
+    };
+  
+    this.http.post('http://localhost:3000/api/verfy', data);
+    
+     
+  }
+}
